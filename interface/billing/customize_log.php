8203033d65a355048b8c80510fd9937ea547243e
stephen waite <stephen.waite@cmsvt.com>
2014-09-12 08:16:05 -0400

    made brady's suggested changes

--- interface/billing/customize_log.php
+++ interface/billing/customize_log.php
@@ -1,25 +1,36 @@
 <?php
-/**
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- */
-     
+/** 
+* interface/billing/customize_log.php - starting point for customization of billing log
+* 
+* LICENSE: This program is free software; you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation; either version 3 
+* of the License, or (at your option) any later version. 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details. 
+* You should have received a copy of the GNU General Public License 
+* along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;. 
+* 
+* @package OpenEMR 
+* @author Stephen Waite <stephen.waite@cmsvt.com> 
+* @author Brady Miller <brady@sparmy.com> 
+* @link http://www.open-emr.org 
+*/     
+
 $fake_register_globals=false;
 $sanitize_all_escapes=true;
      
 require_once("../globals.php");
 
-$site= $_SESSION['site_id'];
-
-$filename = '../../sites/' . $site . '/edi/process_bills.log';
+$filename = $GLOBALS['OE_SITE_DIR'] . '/edi/process_bills.log';
 
 
 $fh = fopen($filename,'r');
 
 while ($line = fgets($fh)) {
-    echo($line);
+    echo(text($line));
     echo("<br />");
     }
     fclose($fh);
