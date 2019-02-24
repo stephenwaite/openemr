ed54c6d118442214b9628fba6a67b67537bbab66
stee <stephen.waite@cmsvt.com>
2019-02-24 18:46:24 -0500

    cqm_valueset help importation

--- sql/5_0_1-to-5_0_2_upgrade.sql
+++ sql/5_0_1-to-5_0_2_upgrade.sql
@@ -809,3 +809,7 @@ ALTER TABLE `prescriptions`
 ALTER TABLE `ar_session`
     MODIFY `patient_id` bigint(20) NOT NULL;
 #EndIf
+
+#IfNotRow4D supported_external_dataloads load_type CQM_VALUESET load_source NIH_VASC load_release_date 2018-10-01 load_filename 2019-ICD-10-CM-Code-Descriptions.zip
+INSERT INTO `supported_external_dataloads` (`load_type`, `load_source`, `load_release_date`, `load_filename`, `load_checksum`) VALUES ('CQM_VALUESET', 'NIH_VSAC', '2017-09-29','ep_ec_only_cms_20170929.xml.zip','38d2e1a27646f2f09fcc389fd2335c50');
+#EndIf
