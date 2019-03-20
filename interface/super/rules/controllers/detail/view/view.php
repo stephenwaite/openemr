dabf16b0aa5a6476b4f82aed8e3d4e99cf8ed5e0
stephen waite <cmswest1@gmail.com>
2019-03-20 18:11:00 -0400

    move view rules to bootstrap

--- interface/super/rules/controllers/detail/view/view.php
+++ interface/super/rules/controllers/detail/view/view.php
@@ -6,68 +6,76 @@
  // as published by the Free Software Foundation; either version 2
  // of the License, or (at your option) any later version.
 
+use OpenEMR\Core\Header;
+
+Header::setupHeader();
+
 $rule = $viewBean->rule ?>
 
-<script language="javascript" src="<?php js_src('detail.js') ?>"></script>
+
+
 <script type="text/javascript">
     var detail = new rule_detail( {editable: <?php echo $rule->isEditable() ? "true":"false"; ?>});
     detail.init();
 </script>
 
-<table class="header">
-  <tr >
-        <td class="title"><?php echo out(xl('Rule Detail')); ?></td>
-        <td>
+<div class="panel panel-default">
+    <div class="panel-heading"><?php echo out(xl('Rule Detail for ') . $rule->title); ?>
+        <div class="panel-body">
             <a href="index.php?action=browse!list" class="iframe_medium css_button" onclick="top.restoreSession()"><span><?php echo out(xl('Back')); ?></span></a>
-        </td>
-  </tr>
-</table>
+        </div>
+    </div>
 
-<div class="rule_detail">
+<div class="panel panel-default">
     <!--         -->
     <!-- summary -->
     <!--         -->
-    <div class="section text">
-        <p class="header">
+        <div class="panel-heading">
             <?php echo out(xl('Summary')); ?>
             <a href="index.php?action=edit!summary&id=<?php echo out($rule->id); ?>"
                class="action_link" id="edit_summary" onclick="top.restoreSession()">(<?php echo out(xl('edit')); ?>)</a>
-        </p>
-        <p><b><?php echo out(xl($rule->title)); ?></b>
+        </div>
+        <div class="panel-body">
+            <p><?php echo out(xl($rule->title)); ?></b>
         (<?php echo implode_funcs(", ", $rule->getRuleTypeLabels(), array( 'xl', 'out' )); ?>)
-        </p>
+           </p>
         <p><?php echo out(xl('Developer')); ?><b>:</b>&nbsp;<?php echo out($rule->developer); ?></p>
         <p><?php echo out(xl('Funding Source')); ?><b>:</b>&nbsp;<?php echo out($rule->funding_source); ?></p>
         <p><?php echo out(xl('Release')); ?><b>:</b>&nbsp;<?php echo out($rule->release); ?></p>
         <p><?php echo out(xl('Web Reference')); ?><b>:</b>&nbsp;<?php echo out($rule->web_ref); ?></p>
-    </div>
+        </div>
+</div>
+
 
     <!--                    -->
     <!-- reminder intervals -->
     <!--                    -->
     <?php $intervals = $rule->reminderIntervals; if ($intervals) { ?>
-    <div class="section text">
-        <p class="header">
+    <div class="container">
+        <h3>
             <?php echo out(xl('Reminder intervals')); ?>
             <a href="index.php?action=edit!intervals&id=<?php echo $rule->id ?>" class="action_link" onclick="top.restoreSession()">(<?php echo out(xl('edit')); ?>)</a>
-        </p>
+        </h3>
 
         <?php if ($intervals->getTypes()) {?>
-        <p>
-            <div>
-                <span class="left_col colhead"><u><?php echo out(xl('Type')); ?></u></span>
-                <span class="end_col colhead"><u><?php echo out(xl('Detail')); ?></u></span>
-            </div>
-
+        <table>
+            <thead>
+              <tr>
+                <th><u><?php echo out(xl('Type')); ?></u></th>
+                <th><u><?php echo out(xl('Detail')); ?></u></th>
+              </tr>
+            </thead>
+            <tbody>
             <?php foreach ($intervals->getTypes() as $type) {?>
-                <div>
-                <span class="left_col"><?php echo out(xl($type->lbl)); ?></span>
-                <span class="end_col">
+                <tr>
+                  <td><?php echo out(xl($type->lbl)); ?></td>
+                  <td>
                     <?php echo out($intervals->displayDetails($type)); ?>
-                </span>
-                </div>
+                  </td>
+                </tr>
             <?php } ?>
-        </p>
+            </tbody>
+        </table>
         <?php } else { ?>
         <p><?php echo out(xl('None defined')); ?></p>
         <?php } ?>
@@ -82,17 +90,17 @@ $rule = $viewBean->rule ?>
         <p class="header"><?php echo out(xl('Demographics filter criteria')); ?> <a href="index.php?action=edit!add_criteria&id=<?php echo out($rule->id); ?>&criteriaType=filter" class="action_link" onclick="top.restoreSession()">(<?php echo out(xl('add')); ?>)</a></p>
         <p>
             <?php if ($filters->criteria) { ?>
-
-                <div>
-                    <span class="left_col">&nbsp;</span>
-                    <span class="mid_col"><u><?php echo out(xl('Criteria')); ?></u></span>
-                    <span class="mid_col"><u><?php echo out(xl('Characteristics')); ?></u></span>
-                    <span class="end_col"><u><?php echo out(xl('Requirements')); ?></u></span>
+<div class="container">
+                <div class="row">
+                    <div class="col-lg-3"><u><?php echo out(xl('Edit')); ?></u></div>
+                    <div class="col-lg-3"><u><?php echo out(xl('Criteria')); ?></u></div>
+                    <div class="col-lg-3"><u><?php echo out(xl('Characteristics')); ?></u></div>
+                    <div class="col-lg-3"><u><?php echo out(xl('Requirements')); ?></u></div>
                 </div>
 
                 <?php foreach ($filters->criteria as $criteria) { ?>
-                    <div>
-                        <span class="left_col">
+                    <div class="row">
+                        <div class="col-lg-3">
                             <a href="index.php?action=edit!filter&id=<?php echo out($rule->id); ?>&guid=<?php echo out($criteria->guid); ?>"
                                class="action_link" onclick="top.restoreSession()">
                                 (<?php echo out(xl('edit')); ?>)
@@ -101,11 +109,12 @@ $rule = $viewBean->rule ?>
                                class="action_link" onclick="top.restoreSession()">
                                 (<?php echo out(xl('delete')); ?>)
                             </a>
-                        </span>
-                        <span class="mid_col"><?php echo( out($criteria->getTitle()) ); ?></span>
-                        <span class="mid_col"><?php echo( out($criteria->getCharacteristics()) ); ?></span>
-                        <span class="end_col"><?php echo( out($criteria->getRequirements()) ); ?></span>
+                        </div>
+                        <div class="col-lg-3"><?php echo( out($criteria->getTitle()) ); ?></div>
+                        <div class="col-lg-3"><?php echo( out($criteria->getCharacteristics()) ); ?></div>
+                        <div class="col-lg-3"><?php echo( out($criteria->getRequirements()) ); ?></div>
                     </div>
+        </div>
                 <?php } ?>
             <?php } else { ?>
                 <p><?php echo out(xl('None defined')); ?></p>
@@ -233,5 +242,4 @@ $rule = $viewBean->rule ?>
         </div>
     
     </div>
-
 </div>
