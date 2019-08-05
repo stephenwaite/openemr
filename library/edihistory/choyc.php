241467476a819356d80a8f89e60a64790abed149
stee <stephen.waite@cmsvt.com>
2019-08-05 08:00:36 -0400

    use edihistory to parse neicwk

---
+++ library/edihistory/choyc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wiggins
+ * Date: 8/4/19
+ * Time: 5:03 PM
+ */
+
+require_once ("edih_x12file_class.php");
+
+$neic = new edih_x12_file("/tmp/neicwk");
+
