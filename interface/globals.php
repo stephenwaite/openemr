47dd2eb4eb57996f3a03d418f0aba6d108fa1a44
stephen waite <stephen.waite@cmsvt.com>
2020-11-11 16:45:16 -0500

    module table work around

--- interface/globals.php
+++ interface/globals.php
@@ -559,25 +559,27 @@ $SMTP_Auth = !empty($GLOBALS['SMTP_USER']);
 
 
 //module configurations
-$GLOBALS['baseModDir'] = "interface/modules/"; //default path of modules
-$GLOBALS['customModDir'] = "custom_modules"; //non zend modules
-$GLOBALS['zendModDir'] = "zend_modules"; //zend modules
+if (!$sql_upgrade_caller) {
+    $GLOBALS['baseModDir'] = "interface/modules/"; //default path of modules
+    $GLOBALS['customModDir'] = "custom_modules"; //non zend modules
+    $GLOBALS['zendModDir'] = "zend_modules"; //zend modules
 
-try {
-    // load up the modules system and bootstrap them.
-    // This has to be fast, so any modules that tie into the bootstrap must be kept lightweight
-    // registering event listeners, etc.
-    // TODO: why do we have 3 different directories we need to pass in for the zend dir path. shouldn't zendModDir already have all the paths set up?
-    /** @var ModulesApplication */
-    $GLOBALS['modules_application'] = new ModulesApplication(
-        $GLOBALS["kernel"],
-        $GLOBALS['fileroot'],
-        $GLOBALS['baseModDir'],
-        $GLOBALS['zendModDir']
-    );
-} catch (\Exception $ex) {
-    error_log(errorLogEscape($ex->getMessage() . $ex->getTraceAsString()));
-    die();
+    try {
+        // load up the modules system and bootstrap them.
+        // This has to be fast, so any modules that tie into the bootstrap must be kept lightweight
+        // registering event listeners, etc.
+        // TODO: why do we have 3 different directories we need to pass in for the zend dir path. shouldn't zendModDir already have all the paths set up?
+        /** @var ModulesApplication */
+        $GLOBALS['modules_application'] = new ModulesApplication(
+            $GLOBALS["kernel"],
+            $GLOBALS['fileroot'],
+            $GLOBALS['baseModDir'],
+            $GLOBALS['zendModDir']
+        );
+    } catch (\Exception $ex) {
+        error_log(errorLogEscape($ex->getMessage() . $ex->getTraceAsString()));
+        die();
+    }
 }
 
 // Don't change anything below this line. ////////////////////////////
--- sql_upgrade.php
+++ sql_upgrade.php
@@ -22,6 +22,7 @@ ini_set('max_execution_time', '0');
 
 $ignoreAuth = true; // no login required
 
+$sql_upgrade_caller = true;
 require_once('interface/globals.php');
 require_once('library/sql_upgrade_fx.php');
 
