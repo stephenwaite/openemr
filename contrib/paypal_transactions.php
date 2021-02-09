6083a316249236568594a3aab0f8c0f820ea08cc
stephen waite <stephen.waite@cmsvt.com>
2021-02-09 05:55:50 -0500

    more

--- contrib/paypal_transactions.php
+++ contrib/paypal_transactions.php
@@ -7,11 +7,41 @@ require_once(dirname(__FILE__) . "/../interface/globals.php");
 
 use League\Csv\Reader;
 
-$csv = Reader::createFromPath('/tmp/test.csv', 'r');
-$csv->setHeaderOffset(0);
-$header_offset = $csv->getHeaderOffset(); //returns 0
-$header = $csv->getHeader(); //returns ['First Name', 'Last Name', 'E-mail']
-echo $header;
+$reader = Reader::createFromPath('/tmp/test.csv', 'r');
+$reader->setHeaderOffset(0);
 
+$records = $reader->getRecords();
+foreach ($records as $offset => $record) {
+    //$offset : represents the record offset
+    //var_export($record); // returns something like
+    //var_dump($record);
+    //if (in_array($record['Date'], $record)) {
+    //echo $record['Date'] . "\n";
+    //echo implode('', array_reverse(explode('/', $record['Date']))) . "\n";
+    if ($record['Type'] == 'Website Payment' && $record['Status'] ==  "Completed") {
+        $status = "PAY";
+
+        $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/", "$3$1$2", $record['Date']);
+
+        $name = $record['Name'];
+        $garno = strtoupper($record['Subject']);
+        $gross = $record['Gross'];
+        $fee   = -1 * $record['Fee'];
+        $net   = $record['Net'];
+        $email = $record['From Email Address'];
+
+        echo $newDate . ",\"" . $name . "\"," . $garno . "," . $gross . "," . $status . ",T,0," . $fee . "," . $net . "\n";
+
+    } else {
+        //var_dump($record);
+    }
+    //}
+    // array(
+    //  'john',
+    //  'doe',
+    //  'john.doe@example.com'
+    // );
+    //
+}
 
 
