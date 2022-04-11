e490249229010c905ce2109d0f8b17b8a7d57170
stephen waite <stephen.waite@cmsvt.com>
2022-04-11 13:54:58 -0400

    default to last 60 days for rmj

--- interface/billing/billing_report.php
+++ interface/billing/billing_report.php
@@ -699,8 +699,15 @@ $partners = $x->_utility_array($x->x12_partner_factory());
                         // It is labled(Included for Insurance ajax criteria)(Line:-279-299).
                         $TPSCriteriaIncludeMaster[1] = "OpenEMR\Billing\BillingReport::insuranceCompanyDisplay";
                         if (!isset($_REQUEST['mode'])) {// default case
-                            $_REQUEST['final_this_page_criteria'][0] = "billing.billed|=|0";
-                            $_REQUEST['final_this_page_criteria_text'][0] = xl("Billing Status = Unbilled");
+                            if ($_SESSION['site_id'] == '200') {
+                                $_REQUEST['final_this_page_criteria'][0] = "form_encounter.date|between|" . date('Y-m-d', mktime(0, 0, 0, date("m")-2, date("d"),   date("Y"))) . "|" . date("Y-m-d 23:59:59");
+                                $_REQUEST['final_this_page_criteria_text'][0] = xl("Date of Service = Last 2 months");
+                                $_REQUEST['final_this_page_criteria'][1] = "billing.billed|=|0";
+                                $_REQUEST['final_this_page_criteria_text'][1] = xl("Billing Status = Unbilled");
+                            } else {
+                                $_REQUEST['final_this_page_criteria'][0] = "billing.billed|=|0";
+                                $_REQUEST['final_this_page_criteria_text'][0] = xl("Billing Status = Unbilled");
+                            }
                             $_REQUEST['date_master_criteria_form_encounter_date'] = "today";
                             $_REQUEST['master_from_date_form_encounter_date'] = date("Y-m-d");
                             $_REQUEST['master_to_date_form_encounter_date'] = date("Y-m-d");
