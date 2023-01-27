fa6d9ea537697c8022f2bbceb7450d40bcdd897d
stephenwaite <stephen.waite@cmsvt.com>
2023-01-27 11:02:43 -0500

    fix usfhp svc facility

--- src/Billing/X125010837P.php
+++ src/Billing/X125010837P.php
@@ -1029,7 +1029,10 @@ class X125010837P
         // End of Loop 2310B
 
         // Loop 2310C is omitted in the case of home visits (POS=12).
-        if ($claim->facilityPOS() != 12 && ($claim->facilityNPI() != $claim->billingFacilityNPI())) {
+        if (
+            $claim->facilityNPI() != $claim->billingFacilityNPI()
+            || $claim->payerID() == '53275'
+            ) {
             ++$edicount;
             $out .= "NM1" .       // Loop 2310C Service Location
                 "*" . "77" .
