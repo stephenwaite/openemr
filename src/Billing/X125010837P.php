e3e40afe2d10f47d9640242547df57ac1ef0502f
stephen waite <stephen.waite@cmsvt.com>
2023-05-31 08:26:43 -0400

    use submitter id instead of tax id

--- src/Billing/X125010837P.php
+++ src/Billing/X125010837P.php
@@ -160,7 +160,7 @@ class X125010837P
                     "*" .
                     "*" .
                     "*" . "46" .
-                    "*" . $claim->billingIdCode();
+                    "*" . $claim->x12_sender_id();
                 // else use provider's group name
                 } else {
                     $billingFacilityName = substr($claim->billingFacilityName(), 0, 60);
