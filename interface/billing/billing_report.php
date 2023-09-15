3a3ac2b420a58af71448e7f261bd698f94976e7b
stephen waite <stephen.waite@cmsvt.com>
2023-09-15 11:26:15 -0400

    fix to patient in billing report

--- interface/billing/billing_report.php
+++ interface/billing/billing_report.php
@@ -359,8 +359,6 @@ $partners = $x->_utility_array($x->x12_partner_factory());
             top.restoreSession();
             paturl = 'patient_file/summary/insurance_edit.php?pid=' + encodeURIComponent(pid);
             parent.left_nav.setPatient(pname, pid, pubpid, '', dobstr);
-            parent.left_nav.loadFrame('ens1', 'enc',
-                'patient_file/history/encounters.php?pid=' + encodeURIComponent(pid));
             parent.left_nav.loadFrame('dem1', 'pat', paturl);
         }
 
