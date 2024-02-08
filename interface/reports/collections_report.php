ba3dfaaff93a9c594465419d0960502cc4524b26
stephen waite <stephen.waite@cmsvt.com>
2024-02-08 13:59:44 -0500

    limit collections export

--- interface/reports/collections_report.php
+++ interface/reports/collections_report.php
@@ -1442,9 +1442,11 @@ if (empty($_POST['form_csvexport'])) {
     <a href='javascript:;' class='btn btn-secondary btn-transmit' onclick='$("#form_csvexport").attr("value","true"); $("#theform").submit();'>
             <?php echo xlt('Export Selected as CSV'); ?>
     </a>
+    <?php if ($_SESSION['site_id'] != '1400') { ?>
     <a href='javascript:;' class='btn btn-secondary btn-transmit' onclick='$("#form_export").attr("value","true"); $("#form_csvexport").val(""); $("#form_clear_ins_debt").val("");$("#theform").submit();'>
             <?php echo xlt('Export Selected to Collections'); ?>
     </a>
+    <? } ?>
     <a href='javascript:;' class='btn btn-secondary btn-transmit' onclick='$("#form_clear_ins_debt").attr("value", "true"); $("#form_export").val(""); $("#form_csvexport").attr("value", "true"); $("#theform").submit();'>
             <?php echo xlt('Clear Insurance Debt'); ?>
     </a>
