8a28de0a62b7b92bc6e1475f81cf9e8478323118
stephen waite <stephen.waite@cmsvt.com>
2025-01-14 18:06:17 -0500

    fix up so it works :)

--- contrib/util/billing/raise_fees.php
+++ contrib/util/billing/raise_fees.php
@@ -11,12 +11,12 @@
 */
 
 // comment this out when using this script (and then uncomment it again when done using script)
-exit;
+//exit;
 
 if (php_sapi_name() !== 'cli') {
     echo "Only php cli can execute command\n";
-    echo "example use: php default 03 false\n";
-    echo "will rais fees by 3% if set to true\n";
+    echo "example use: php default 3 0\n";
+    echo "will raise fees by 3% if set to true\n";
     die;
 }
 
@@ -24,7 +24,7 @@ $_GET['site'] = $argv[1] ?? 'default';
 $ignoreAuth = true;
 require_once __DIR__ . "/../../../interface/globals.php";
 
-$feeBump = 1 + floatval($argv[2] ?? 0);
+$feeBump = 1 + (.01 * floatval($argv[2] ?? 0));
 $liveRun = boolval($argv[3] ?? 0);
 
 $codes_sql = sqlStatement("SELECT * FROM `codes`");
@@ -48,8 +48,8 @@ while ($code = sqlFetchArray($codes_sql)) {
     $format = "raise existing fee by of %7.2f for %5s : %2s to %7.2f";
     echo sprintf($format, $ourFee, $ourCode, $ourMod, $newFee) . "\n";
     if ($liveRun) {
-        echo "update prices table for code $our_code:$our_mod from " . $our_fee .
-            " to " . $ceil_fee . " with price id " . $price_id . "\n";
+        echo "update prices table for code $ourCode:$ourMod from " . $ourFee .
+            " to " . $newFee . " with price id " . $priceId . "\n";
         $update_prices = sqlQuery("UPDATE `prices` SET `pr_price` = ? WHERE `pr_id` = ?", [$newFee, $priceId]);
     }
 }
