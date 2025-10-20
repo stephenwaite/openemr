5f0076f484d5eabb8730287760a897fb62c13967
stephen waite <stephen.waite@cmsvt.com>
2025-10-20 16:17:01 -0400

    ngs password fix

--- contrib/multisite/updateNgsPassword.php
+++ contrib/multisite/updateNgsPassword.php
@@ -3,12 +3,27 @@
 /*
 * php updateNgsPassword <site> <password> <username>
 */
-exit;
+// Enable this script via environment variable
+if (!getenv('OPENEMR_ENABLE_NGS_PASSWORD')) {
+    die('Set OPENEMR_ENABLE_NGS_PASSWORD=1 environment variable to enable this script');
+}
+
+if (php_sapi_name() !== 'cli') {
+    echo "Only php cli can execute command\n";
+    echo "example use: php default login password \n";
+    die;
+}
+
 $_GET['site'] = $argv[1];
 $ignoreAuth = true;
-require_once __DIR__ . "/../../interface/globals.php";
+require_once __DIR__ . "/../../../interface/globals.php";
+
+use League\Csv\Reader;
 
 use OpenEMR\Common\Crypto\CryptoGen;
+
+$ngsLogin = $argv[2];
+$newPassword = $argv[3];
 
 try {
     $autoSftpFlag = sqlQuery("SELECT `gl_value` FROM `globals` WHERE `gl_name` = 'auto_sftp_claims_to_x12_partner'");
@@ -19,13 +34,17 @@ try {
 if (!empty($autoSftpFlag)) {
     try {
         $cryptoGen = new CryptoGen();
-        $encryptedPassword = $cryptoGen->encryptStandard($argv[2]);
-        $sqlStatement = "UPDATE `x12_partners` SET `x12_sftp_pass` = ? WHERE `x12_sftp_login` = ?";
-        sqlStatement($sqlStatement, [$encryptedPassword, $argv[3]]);
-        echo ("updated NGS Password \n");
+        $encryptedPassword = $cryptoGen->encryptStandard($newPassword);
+        $x12PartnerId = sqlQuery("SELECT `id` FROM `x12_partners` WHERE `x12_sftp_login` = ?", [$ngsLogin])['id'];
+        $sqlStatement = "UPDATE `x12_partners` SET `x12_sftp_pass` = ? WHERE `id` = ?";
+        if (sqlStatement($sqlStatement, [$encryptedPassword, $x12PartnerId])) {
+            echo "updated NGS Password :) \n";
+        } else {
+            echo "errored :( \n";
+        }
     } catch (Exception $e) {
         // error stuff goes here
-        echo ("failed to update NGS Password \n");
+        echo ("exception: failed to update NGS Password \n");
     }
 } else {
     echo ("autoSftpFlag is not set \n");
